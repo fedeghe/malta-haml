4ca58700cb330333e8c6c73386f4bf43efae1855
fedeghe <fedeghe@gmail.com>
2019-07-06 23:04:46 +0200

    reject set

--- index.js
+++ index.js
@@ -25,24 +25,25 @@ function malta_haml(o, options) {
 	}
 
 	// convert tabss to 2 spaces
-	// 
 	o.content = o.content.replace(/\t/gm, "  ");
-
-	try {
-		o.content = haml.render(o.content+'', {locals : data});
-	} catch (err) {
-		self.doErr(err, o, pluginName);
-	}
 	o.name = o.name.replace(/\.haml/, '.html');
 
 	return function (solve, reject){
-		fs.writeFile(o.name, o.content, function(err) {	
-			err && self.doErr(err, o, pluginName);
-			msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
-			fs.unlink(oldname, () => {});
-			solve(o);
-			self.notifyAndUnlock(start, msg);
-		});
+        try {
+            o.content = haml.render(o.content+'', {locals : data});
+            fs.writeFile(o.name, o.content, function(err) {	
+                err && self.doErr(err, o, pluginName);
+                msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
+                fs.unlink(oldname, () => {});
+                err
+                    ? reject(`Plugin ${pluginName} write error:\n${err}`)
+                    : solve(o);
+                self.notifyAndUnlock(start, msg);
+            });
+        } catch (err) {
+            reject(`Plugin ${pluginName} compilation error:\n${err}`)
+            self.doErr(err, o, pluginName);
+        }
 	};
 }
 malta_haml.ext = 'haml';
